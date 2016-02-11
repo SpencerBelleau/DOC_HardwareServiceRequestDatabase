6c8c46b729bf2e0c7602e69dbc178ebe2a658592
Spencer Belleau <spencer.belleau@gmail.com>
2016-02-10 17:01:45 -0800

    Changed a bunch of stuff, now creates a somewhat working system. Needs proper login screen.

--- Index.php
+++ Index.php
@@ -5,9 +5,20 @@
 	
 	if (isset($_POST['submission']))
 	{
-		echo "New submission made";
-		$com2 = "INSERT INTO DOC_Hardware_Records (firstName, lastName, computerModel, serviceRequested, extraParts, complete) VALUES (:firstName, :lastName, :computerModel, :serviceRequested, :extraParts, 0)";
-		executeSQL_Safe_U($com2, $dbConn, ":firstName", $_POST['firstName'], ":lastName", $_POST['lastName'], ":computerModel", $_POST['computerModel'], ":serviceRequested", $_POST['serviceRequested'], ":extraParts", $_POST['extraParts']);
+		if($_POST['submission'] == "New")
+		{
+			echo "New submission made";
+			$com2 = "INSERT INTO DOC_Hardware_Records (firstName, lastName, address, phoneNumber, email, computerModel, serviceRequested, extraParts, complete) VALUES (:firstName, :lastName, :address, :phoneNumber, :email, :computerModel, :serviceRequested, :extraParts, 0)";
+			executeSQL_Safe_U($com2, $dbConn, ":firstName", $_POST['firstName'], ":lastName", $_POST['lastName'], ":address", $_POST['address'], ":phoneNumber", $_POST['phoneNumber'], ":email", $_POST['email'], ":computerModel", $_POST['computerModel'], ":serviceRequested", $_POST['serviceRequested'], ":extraParts", $_POST['extraParts']);
+		}else if($_POST['submission'] == "Complete"){
+			echo "Service ticket " . $_POST['serviceID'] . " updated.";
+			$com2 = "UPDATE DOC_Hardware_Records SET complete=2 WHERE serviceID=:id";
+			executeSQL_Safe_U($com2, $dbConn, ":id", $_POST['serviceID']);
+		}else if($_POST['submission'] == "Open"){
+			echo "Service ticket " . $_POST['serviceID'] . " updated.";
+			$com2 = "UPDATE DOC_Hardware_Records SET complete=1 WHERE serviceID=:id";
+			executeSQL_Safe_U($com2, $dbConn, ":id", $_POST['serviceID']);
+		}
 	}
 ?>
 <!DOCTYPE html>
@@ -18,33 +29,76 @@
 	table{
 		margin-left:auto;
 		margin-right:auto;
+		border:1px solid black;
+		border-radius:4px;
+		width:450px;
 	}
 </style>
 <body>
-<h1>TEST</h1>
+<h1>Digital Otter Center Hardware Service Request Form</h1>
 
-<table border=1>
+<table style="background-color:lightblue">
 	<form method="post">
-	<input type="hidden" name="submission" val="true">
+	<input type="hidden" name="submission" value="New">
 	<tr>
 		<td colspan=2>
 			<h3 style="text-align:center">New Service Request</h3>
 		</td>
 	</tr>
 	<tr>
-		<td>
-			First Name: <input type="text" name="firstName">
+		<td style="width:150px">
+			First Name:
 		</td>
 		<td>
-			Last Name: <input type="text" name="lastName">
+			<input style="width:98%" type="text" name="firstName">
 		</td>
 	</tr>
 	<tr>
-		<td>
-			Computer Model: <input type="text" name="computerModel">
+		<td style="width:150px">
+			Last Name:
 		</td>
 		<td>
-			Service Requested: <input type="text" name="serviceRequested">
+			<input style="width:98%" type="text" name="lastName">
+		</td>
+	</tr>
+	<tr>
+		<td style="width:150px">
+			Address:
+		</td>
+		<td>
+			<input style="width:98%" type="text" name="address">
+		</td>
+	</tr>
+	<tr>
+		<td style="width:150px">
+			Phone Number:
+		</td>
+		<td>
+			<input style="width:98%" type="text" name="phoneNumber">
+		</td>
+	</tr>
+	<tr>
+		<td style="width:150px">
+			E-mail:
+		</td>
+		<td>
+			<input style="width:98%" type="text" name="email">
+		</td>
+	</tr>
+	<tr>
+		<td style="width:150px">
+			Computer Model:
+		</td>
+		<td>
+			<input style="width:98%" type="text" name="computerModel">
+		</td>
+	</tr>
+	<tr>
+		<td style="width:150px">
+			Service Requested:
+		</td>
+		<td>
+			<input style="width:98%" type="text" name="serviceRequested">
 		</td>
 	</tr>
 	<tr>
@@ -62,18 +116,36 @@
 
 <?php
 	echo '<h2> Existing Service Requests </h2>';
-	$com1 = "SELECT * FROM DOC_Hardware_Records";
+	$com1 = "SELECT * FROM DOC_Hardware_Records ORDER BY complete ASC, requestTime ASC";
 	$fetched = executeSQL($com1, $dbConn);
 	foreach($fetched as $a)
 	{
+		drawTicket($a);
+		echo '<br/>';
+		/*
 		if($a['complete'] == 0)
 		{
-			$a['complete'] = "false";
+			$col = "style='background-color:LightCoral'";
 		}else{
-			$a['complete'] = "true";
+			$col = "style='background-color:LimeGreen'";
 		}
+		echo '<table border=1>';
+		echo '<tr><td ' . $col . '>' . $a['firstName'] . '</td><td ' . $col . '>' . $a['lastName'] . '</td></tr>';
+		echo '<tr><td colspan=2 ' . $col . '>' . $a['address'] . '</td></tr>';
+		echo '<tr><td ' . $col . '>' . $a['phoneNumber'] . '</td><td ' . $col . '>' . $a['email'] . '</td></tr>';
+		echo '<tr><td ' . $col . '>' . $a['computerModel'] . '</td><td ' . $col . '>' . $a['serviceRequested'] . '</td></tr>';
+		echo '<tr><td colspan=2 ' . $col . '>' . $a['extraParts'] . '</td></tr>';
+		if($a['complete'] == 0)
+		{
+			echo '<tr><form method="post"><td colspan=2>';
+			echo '<input type="hidden" name="submission" value="Update">';
+			echo '<input type="hidden" value="'. $a['serviceID'] .'" name="serviceID">';
+			echo '<input style="width:100%" type="submit" value="Complete">';
+			echo '</td></form></tr>';
+		}
+		echo '</table>';*/
 	}
-	SQLTable($fetched, array("ID", "First Name", "Last Name", "Computer Model", "Service Requested", "Extra Parts", "Finished", "Time Requested"));
+	//SQLTable($fetched, array("ID", "First Name", "Last Name", "Computer Model", "Service Requested", "Extra Parts", "Finished", "Time Requested"));
 ?>
 
 </body>
--- snippets/utils.php
+++ snippets/utils.php
@@ -16,4 +16,46 @@
 		$t = sha1($input);
 		return substr($t, 0, $length);
 	}
+	
+	function drawTicket($row)
+	{
+		if($row['complete'] == 0)
+		{
+			$col = "style='background-color:LightCoral'";
+		}else if ($row['complete'] == 2){
+			$col = "style='background-color:LimeGreen'";
+		}else{
+			$col = "style='background-color:Khaki'";
+		}
+		echo '<table ' . $col . '>';
+		echo '<tr><td style="width:150px"><b>NAME:</b></td><td> ' . $row['firstName'] . " " . $row['lastName'] . '</td></tr>';
+		echo '<tr><td style="width:150px"><b>ADDRESS:</b></td><td> ' . $row['address'] . '</td></tr>';
+		echo '<tr><td style="width:150px"><b>PHONE:</b></td><td> ' . $row['phoneNumber'] . '</td></tr>';
+		echo '<tr><td style="width:150px"><b>E-MAIL:</b></td><td> ' . $row['email'] . '</td></tr>';
+		echo '<tr><td style="width:150px"><b>COMPUTER:</b></td><td> ' . $row['computerModel'] . '</td></tr>';
+		echo '<tr><td style="width:150px"><b>SERVICE:</b></td><td> ' . $row['serviceRequested'] . '</td></tr>';
+		echo '<tr><td colspan=2><b>EXTRA PARTS</b><br/>' . $row['extraParts'] . '</td></tr>';
+		echo '<tr><td style="width:150px"><b>TIME SUBMITTED:</b></td><td> ' . $row['requestTime'] . '</td></tr>';
+		if($row['complete'] == 0)
+		{
+			echo '<tr><form method="post"><td style="width:50%">';
+			echo '<input type="hidden" name="submission" value="Open">';
+			echo '<input type="hidden" value="'. $row['serviceID'] .'" name="serviceID">';
+			echo '<input style="width:100%" type="submit" value="Open">';
+			echo '</td></form>';
+			
+			echo '<form method="post"><td style="width:50%">';
+			echo '<input type="hidden" name="submission" value="Complete">';
+			echo '<input type="hidden" value="'. $row['serviceID'] .'" name="serviceID">';
+			echo '<input style="width:100%" type="submit" value="Complete">';
+			echo '</td></form></tr>';
+		}else if($row['complete'] == 1){
+			echo '<tr><form method="post"><td colspan=2>';
+			echo '<input type="hidden" name="submission" value="Complete">';
+			echo '<input type="hidden" value="'. $row['serviceID'] .'" name="serviceID">';
+			echo '<input style="width:100%" type="submit" value="Complete">';
+			echo '</td></form></tr>';
+		}
+		echo '</table>';
+	}
 ?>
